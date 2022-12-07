74a600630301ce718abfbf7ef9ba5e9b328f0e3d
bill <billanderson887@gmail.com>
2022-12-07 09:38:23 -0600

    Solved Day 6 b

--- src/Day6/day6.py
+++ src/Day6/day6.py
@@ -1,11 +1,12 @@
 if __name__ == '__main__':
     file_input = open('signal.txt')
     signal = file_input.read()
+    marker_length = 14
     index = 0
     for i in range(len(signal)):
-        if i < 4:
+        if i < marker_length:
             continue
-        potential_start = list(signal[i-4:i])
+        potential_start = list(signal[i-marker_length:i])
         potential_start_set = set(potential_start)
         if len(potential_start) == len(potential_start_set):
             index = i
